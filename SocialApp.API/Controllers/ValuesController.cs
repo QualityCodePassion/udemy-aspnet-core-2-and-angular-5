908581bc5acf1f7186e8fd473d48f9578e5e248d
QualityCodePassion <hale_timo@hotmail.com>
2018-01-08 18:30:48 +1100

    Getting values via the controller asynchronously

--- SocialApp.API/Controllers/ValuesController.cs
+++ SocialApp.API/Controllers/ValuesController.cs
@@ -3,24 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialApp.API.Data;
 
 namespace SocialApp.API.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+
+        }
+
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IActionResult> GetValues()
         {
-            return new string[] { "value1", "value2" };
+             var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            return "value";
+            var value = await _context.Values.FirstOrDefaultAsync( x => x.Id == id);
+
+            return Ok(value);
         }
 
         // POST api/values
