2f673bad70b0b1c898e375082aff2aa6ad74f317
QualityCodePassion <hale_timo@hotmail.com>
2018-01-21 12:35:26 +1100

    [SPA] Added a basic angular 5 navbar to SPA, but not connected yet.

--- SocialApp.SPA/src/app/app.module.ts
+++ SocialApp.SPA/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import {FormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -5,16 +6,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent
+    ValueComponent,
+    NavComponent
 ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
